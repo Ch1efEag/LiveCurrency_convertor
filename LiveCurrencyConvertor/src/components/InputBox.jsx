9cd47291c5534431183c50b07233cd5b9a76290f
Harsh Gaur <harshgr441@gmail.com>
2025-01-20 19:31:53 +0530

    project_created-_hook_created

---
+++ LiveCurrencyConvertor/src/components/InputBox.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function InputBox(){
+
+    return (<>
+    <div>
+        <div>
+            <label></label>
+            <input/>
+        </div>
+
+        <div>
+    <p></p>
+    <select>
+
+    </select>
+        </div>
+    </div>
+    </>);
+}
+export default InputBox;
