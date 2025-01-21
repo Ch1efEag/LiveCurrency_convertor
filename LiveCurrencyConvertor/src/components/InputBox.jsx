fd61fc7ec53449ae9ecac67744d660e205b3e337
Harsh Gaur <harshgr441@gmail.com>
2025-01-21 16:37:03 +0530

    fixed css

--- LiveCurrencyConvertor/src/components/InputBox.jsx
+++ LiveCurrencyConvertor/src/components/InputBox.jsx
@@ -2,32 +2,37 @@ import React from "react";
 
 function InputBox({
     label,
+    amount,
     amountDisabled = false,
     onAmountChanged,
     onCurrencyChanged,
+    selectedcurrency = "usd",
     currencyOptions,
-    currencyDisabled = false
+    currencyDisabled = false,
+    className = ""
 
 }){
 
     return (<>
-    <div>
-        <div>
-            <label>
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+        <div className="w-1/2">
+            <label  className="text-black/40 mb-2 inline-block">
                 {label}
             </label>
             <input
             type = "number"
             placeholder="Amount"
+             className="outline-none w-full bg-transparent py-1.5"
             disabled = {amountDisabled}
             value = {amount}
             onChange={(e) => onAmountChanged && onAmountChanged(Number(e.target.value))}
             />
         </div>
 
-        <div>
-    <p>Currency Type</p>
+        <div className="w-1/2 flex flex-wrap justify-end text-right">
+    <p className="text-black/40 mb-2 w-full">Currency Type</p>
     <select
+     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
         value = {selectedcurrency}
         onchange = {(e) => onCurrencyChanged && onCurrencyChanged(e.target.value)}
         disabled = {currencyDisabled}
